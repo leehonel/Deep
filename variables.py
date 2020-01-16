7a33731f07a7d3befa312d5363e7a511a862db4f
Leon Leeh <leonelvis54@gmail.com>
2020-01-16 15:10:17 +0300

    day three

---
+++ variables.py
@@ -0,0 +1,34 @@
+date = 1612020
+day = 'Thursday';
+time = '1300h';
+
+print (date)
+print (day);
+print (time);
+
+counter=100
+print (counter)
+
+a, b, c = 1, 2, 'hello'
+#del a
+print (a) 
+#del a
+print (b) 
+print (c)
+#print (a)
+print ('\n\n')
+
+#strings
+str = 'Ssup world!\t'
+print (str)
+print (str[2])	#prints the third letter of the string
+print (str * 10)	#prints the string 10 times
+print (str + 'Hello World')
+
+
+#lists
+print ('\n\n')
+list = ['past', 'present-', 'future']
+print (list)
+print (list[0])	#this will print the first element of the list
+print (list[1] * 5)	#this will print the second element of the list five times
