2fc9fe94d286f700ef617ce10034acda4a6fbf63
Leon Leeh <leonelvis54@gmail.com>
2020-04-06 01:47:19 +0300

    UI with JavaFx

---
+++ Java/AntProjects/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,53 @@
+/*
+    The main class for a JavaFX application extends the javafx.application.Application class. 
+    The start() method is the main entry point for all JavaFX applicaions.
+    A JavaFX application defines the UI container by means of a stage and a scene. 
+        The Stage class is the top-level JavaFX container. 
+        The Scene class is the container for all content. 
+*/
+package helloworld;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author backey
+ */
+public class HelloWorld extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
